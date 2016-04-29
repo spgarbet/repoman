3c14e3706aa33169b69b40ca3c229a115752286e
Shawn Garbett <Shawn.P.Garbett@Vanderbilt.edu>
2016-04-29 12:45:38 -0500

    More test cases of AST

--- table-grammar/parser.R
+++ table-grammar/parser.R
@@ -259,5 +259,32 @@ test.parserExpr <- function()
    checkEquals(ast$right$right$value,  "spiders")
 }
 
+test.parserRExpr <- function() 
+{
+  pr  <- Parser$new()
+  ast <- pr$run("x+I(10^23/([[rough]]~2)+3) ~ y")
+  
+  checkEquals(ast$symbol, "table")
+  checkEquals(ast$left$symbol, "plus")
+  checkEquals(ast$left$left$symbol, "name")
+  checkEquals(ast$left$left$value, "x")
+  checkEquals(ast$left$right$symbol, "r_expr")
+  checkEquals(ast$left$right$value, "10^23/([[rough]]~2)+3")
+}
 
-#ast2 <- pr$run("I(10^23*(rough+2)+3) ~ spiders + (youth ~ age)+more")
+test.parserEmbeddedTable <- function()
+{
+  pr  <- Parser$new()
+  ast <- pr$run("y ~ (i ~ (j~k) ) + x")
+  
+  checkEquals(ast$symbol, "table")
+  checkEquals(ast$right$symbol, "plus")
+  checkEquals(ast$right$left$symbol, "table")
+  checkEquals(ast$right$left$left$symbol, "name")
+  checkEquals(ast$right$left$left$value,  "i")
+  checkEquals(ast$right$left$right$symbol, "table")
+  checkEquals(ast$right$left$right$left$symbol, "name")
+  checkEquals(ast$right$left$right$left$value, "j")
+  checkEquals(ast$right$left$right$right$symbol, "name")
+  checkEquals(ast$right$left$right$right$value, "k")
+}
